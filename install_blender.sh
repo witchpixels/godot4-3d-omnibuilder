1db25573045696856609bad0d080c401a4b566d3
witchpixels <thewitch@witchpixels.dev>
2023-09-12 21:59:23 -0700

    downgrade blender to 3.5.0

--- install_blender.sh
+++ install_blender.sh
@@ -2,7 +2,7 @@
 
 apt install xz-utils
 
-wget https://download.blender.org/release/Blender3.6/blender-3.6.0-linux-x64.tar.xz
-tar -xJf ./blender-3.6.0-linux-x64.tar.xz
+wget https://download.blender.org/release/Blender3.5/blender-3.5.0-linux-x64.tar.xz
+tar -xJf ./blender-3.5.0-linux-x64.tar.xz
 
-mv ./blender-3.6.0-linux-x64 /opt/blender
+mv ./blender-3.5.0-linux-x64 /opt/blender
