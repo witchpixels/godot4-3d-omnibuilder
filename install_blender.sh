2d0772ed14c81f7118447ef6e121255107a42249
witchpixels <thewitch@witchpixels.dev>
2023-09-12 21:49:01 -0700

    downgrade blender

--- install_blender.sh
+++ install_blender.sh
@@ -2,7 +2,7 @@
 
 apt install xz-utils
 
-wget https://mirrors.ocf.berkeley.edu/blender/release/Blender3.6/blender-3.6.2-linux-x64.tar.xz
-tar -xJf ./blender-3.6.2-linux-x64.tar.xz
+wget https://download.blender.org/release/Blender3.6/blender-3.6.0-linux-x64.tar.xz
+tar -xJf ./blender-3.6.0-linux-x64.tar.xz
 
-mv ./blender-3.6.2-linux-x64 /opt/blender
+mv ./blender-3.6.0-linux-x64 /opt/blender
