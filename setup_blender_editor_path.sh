5b86a5943ea90a1ca20ee3e8ef1b5cf716fec6fa
witchpixels <thewitch@witchpixels.dev>
2023-09-14 12:52:36 -0700

    set default rpc path?

--- setup_blender_editor_path.sh
+++ setup_blender_editor_path.sh
@@ -4,7 +4,7 @@ echo "EDITOR SETTINGS BEFORE ---------------------------------"
 cat ~/.config/godot/editor_settings-4.tres
 echo "-------------------------------------------------------"
 
-echo "filesystem/import/blender/rpc_port = 0" >> ~/.config/godot/editor_settings-4.tres
+echo "filesystem/import/blender/rpc_port = 6011" >> ~/.config/godot/editor_settings-4.tres
 echo "filesystem/import/blender/rpc_server_uptime = 5" >> ~/.config/godot/editor_settings-4.tres
 echo "filesystem/import/blender/blender3_path = \"$(dirname $(which blender))\"" >> ~/.config/godot/editor_settings-4.tres
 
