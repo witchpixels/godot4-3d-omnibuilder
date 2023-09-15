75773eff9233395b8c702d102f2ec71f819daa7a
witchpixels <thewitch@witchpixels.dev>
2023-09-15 14:27:38 -0700

    ensure we always write to /root/.config

--- setup_blender_editor_path.sh
+++ setup_blender_editor_path.sh
@@ -4,13 +4,13 @@ godot -v -e --quit --headless
 
 echo $(which blender)
 echo "EDITOR SETTINGS BEFORE ---------------------------------"
-cat ~/.config/godot/editor_settings-4.tres
+cat /root/.config/godot/editor_settings-4.tres
 echo "-------------------------------------------------------"
 
-echo "filesystem/import/blender/rpc_port = 6011" >> ~/.config/godot/editor_settings-4.tres
-echo "filesystem/import/blender/rpc_server_uptime = 500" >> ~/.config/godot/editor_settings-4.tres
-echo "filesystem/import/blender/blender3_path = \"$(dirname $(which blender))\"" >> ~/.config/godot/editor_settings-4.tres
+echo "filesystem/import/blender/rpc_port = 6011" >> /root/.config/godot/editor_settings-4.tres
+echo "filesystem/import/blender/rpc_server_uptime = 500" >> /root/.config/godot/editor_settings-4.tres
+echo "filesystem/import/blender/blender3_path = \"$(dirname $(which blender))\"" >> /root/.config/godot/editor_settings-4.tres
 
 echo "EDITOR SETTINGS AFTER ---------------------------------"
-cat ~/.config/godot/editor_settings-4.tres
+cat /root/.config/godot/editor_settings-4.tres
 echo "-------------------------------------------------------"
