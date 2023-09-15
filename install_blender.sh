4a06276ce73c583066bba60f27d66a4925468493
witchpixels <thewitch@witchpixels.dev>
2023-09-14 19:42:28 -0700

    install 3.6.0

--- install_blender.sh
+++ install_blender.sh
@@ -3,7 +3,7 @@
 apt install -y xorg
 apt install -y xz-utils
 
-wget https://download.blender.org/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
-tar -xJf ./blender-3.0.0-linux-x64.tar.xz
+wget https://download.blender.org/release/Blender3.6/blender-3.6.0-linux-x64.tar.xz
+tar -xJf ./blender-3.6.0-linux-x64.tar.xz
 
-mv ./blender-3.0.0-linux-x64 /opt/blender
+mv ./blender-3.6.0-linux-x64 /opt/blender
