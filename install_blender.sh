6b0321c112dba65f18dda69b4178c809e09d2f45
witchpixels <thewitch@witchpixels.dev>
2023-09-14 19:17:03 -0700

    install xorg which blender needs to start even headless

--- install_blender.sh
+++ install_blender.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 
+apt install xorg
 apt install xz-utils
 
 wget https://download.blender.org/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
--- setup_blender_editor_path.sh
+++ setup_blender_editor_path.sh
@@ -5,7 +5,7 @@ cat ~/.config/godot/editor_settings-4.tres
 echo "-------------------------------------------------------"
 
 echo "filesystem/import/blender/rpc_port = 6011" >> ~/.config/godot/editor_settings-4.tres
-echo "filesystem/import/blender/rpc_server_uptime = 5" >> ~/.config/godot/editor_settings-4.tres
+echo "filesystem/import/blender/rpc_server_uptime = 500" >> ~/.config/godot/editor_settings-4.tres
 echo "filesystem/import/blender/blender3_path = \"$(dirname $(which blender))\"" >> ~/.config/godot/editor_settings-4.tres
 
 echo "EDITOR SETTINGS AFTER ---------------------------------"
