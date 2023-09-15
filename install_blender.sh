4cd81aca81efc77e9a1f4e7ecb51ca5a9e5b0944
witchpixels <thewitch@witchpixels.dev>
2023-09-14 19:31:17 -0700

    add -y

--- install_blender.sh
+++ install_blender.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
-apt install xorg
-apt install xz-utils
+apt install -y xorg
+apt install -y xz-utils
 
 wget https://download.blender.org/release/Blender3.0/blender-3.0.0-linux-x64.tar.xz
 tar -xJf ./blender-3.0.0-linux-x64.tar.xz
