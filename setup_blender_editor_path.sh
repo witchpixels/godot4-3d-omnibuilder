7b2ee1c586d897d474a0f834f1a137c20d8f7c3a
Terrence <terrence@agilereaction.com>
2025-03-13 17:22:12 -0400

    update workflows and scripts to use Godot version 4.4

--- setup_blender_editor_path.sh
+++ setup_blender_editor_path.sh
@@ -4,13 +4,13 @@ godot -v -e --quit --headless
 
 echo $(which blender)
 echo "EDITOR SETTINGS BEFORE ---------------------------------"
-cat /root/.config/godot/editor_settings-4.3.tres
+cat /root/.config/godot/editor_settings-4.4.tres
 echo "-------------------------------------------------------"
 
-echo "filesystem/import/blender/rpc_port = 6011" >> /root/.config/godot/editor_settings-4.3.tres
-echo "filesystem/import/blender/rpc_server_uptime = 500" >> /root/.config/godot/editor_settings-4.3.tres
-echo "filesystem/import/blender/blender_path = \"$(which blender)\"" >> /root/.config/godot/editor_settings-4.3.tres
+echo "filesystem/import/blender/rpc_port = 6011" >> /root/.config/godot/editor_settings-4.4.tres
+echo "filesystem/import/blender/rpc_server_uptime = 500" >> /root/.config/godot/editor_settings-4.4.tres
+echo "filesystem/import/blender/blender_path = \"$(which blender)\"" >> /root/.config/godot/editor_settings-4.4.tres
 
 echo "EDITOR SETTINGS AFTER ---------------------------------"
-cat /root/.config/godot/editor_settings-4.3.tres
+cat /root/.config/godot/editor_settings-4.4.tres
 echo "-------------------------------------------------------"
